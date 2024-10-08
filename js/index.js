d785456e6522d18983e4f48edb1ada7b7ce071e0
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-10-08 15:57:19 +0200

    Fixed index.js and pizzas.json

--- js/index.js
+++ js/index.js
@@ -10,6 +10,22 @@ fetchPizzasJson().then(pizza=>{
     for (let index=0; index<pizza.pizzas.length; index++){
         let price = pizza.pizzas[index].price;
         let pizzaName = pizza.pizzas[index].name;
-        console.log(pizzaName + "-" + price);
-    }
-});
+        let image = pizza.pizzas[index].image;
+        let ingredients = pizza.pizzas[index].ingredients;
+        pizzaSection.innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <img src=${image} class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${pizzaName}</h5>
+            <p class="card-text">${ingredients}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${price} euros</li>
+            <li class="list-group-item"></li>
+            <li class="list-group-item"></li>
+        </ul>
+        <div class="card-body">
+            <a href="#" class="card-link"></a>
+            <a href="#" class="card-link"></a>
+        </div>
+    </div>`}});
