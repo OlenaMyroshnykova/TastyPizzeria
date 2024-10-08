6bd7dd80bbdb8b97f944036d20005fae3ce663e5
tizzifona <tizzifona@gmail.com>
2024-10-09 01:13:15 +0200

    Fixes for link json

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const requestURL = '/json/pizzas.json';
+const requestURL = 'json/pizzas.json';
 
 async function fetchPizzasJson(){
     const response = await fetch(requestURL);
@@ -6,28 +6,29 @@ async function fetchPizzasJson(){
     return pizzas;
 }
 
-fetchPizzasJson().then(pizza=>{
-    for (let index=0; index<pizza.pizzas.length; index++){
+fetchPizzasJson().then(pizza => {
+    for (let index = 0; index < pizza.pizzas.length; index++) {
         let price = pizza.pizzas[index].price;
         let pizzaName = pizza.pizzas[index].name;
         let image = pizza.pizzas[index].image;
         let ingredients = pizza.pizzas[index].ingredients;
 
         pizzaSection.innerHTML += `
-        <div class="card" style="width: 18rem;">
-            <img src=${image} class="card-img-top" alt="...">
+            <div class="card" style="width: 18rem;">
+                <img src=${image} class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${pizzaName}</h5>
+                <p class="card-text">${ingredients}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${price} euros</li>
+                <li class="list-group-item"></li>
+                <li class="list-group-item"></li>
+            </ul>
             <div class="card-body">
-            <h5 class="card-title">${pizzaName}</h5>
-            <p class="card-text">${ingredients}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">${price} euros</li>
-            <li class="list-group-item"></li>
-            <li class="list-group-item"></li>
-        </ul>
-        <div class="card-body">
-            <a href="#" class="card-link"></a>
-            <a href="#" class="card-link"></a>
-        </div>
-    </div>`
-}});
+                <a href="#" class="card-link"></a>
+                <a href="#" class="card-link"></a>
+            </div>
+        </div>`;
+    }
+});
